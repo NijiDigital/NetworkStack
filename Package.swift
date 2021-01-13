fe6ae51791f19b94d0b979d96d4e4c1dd29d5399
Erwan Bouet <erwan.bouet@niji.fr>
2021-01-13 14:36:29 +0100

    fix(swift-package): added RxCocoa dependency as a product from rxSwift

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
       name: "NetworkStack",
-      dependencies: ["RxSwift",
+      dependencies: ["RxSwift", .product(name: "RxCocoa", package: "RxSwift"),
                      "Alamofire", "KeychainAccess"]),
     .testTarget(
       name: "NetworkStackTests",
